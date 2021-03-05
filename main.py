5c9618fe7c15ddf87bb484d3ef270cf684fc0b4b
sirex <sirex3715@gmail.com>
2021-03-05 22:29:05 +0900

    add comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+# ボタン表示を分かりやすくするためリストで格納
 BUTTON_TEXT = ["7", "8", "9", "+", "AC",
                "6", "5", "4", "-", "BS",
                "3", "2", "1", "*", "Undo",
@@ -10,6 +11,8 @@ class App(tk.Tk):
         super().__init__()
 
         self.title("電卓")
+
+        # 表示盤を表現
         self.upper_frame = tk.Frame(self, bg="black")
         self.text = tk.StringVar()
         self.text.set("")
@@ -17,9 +20,7 @@ class App(tk.Tk):
                               background="black", foreground="white").pack(fill=tk.X)
         self.upper_frame.pack(padx=10, pady=10)
 
-        self.output_strings = ""
-        self.most_recent_strings = ""
-
+        # ボタンを表現
         self.lower_frame = tk.Frame(self, bg="black")
 
         for i, t in zip(range(20), BUTTON_TEXT):
@@ -29,29 +30,34 @@ class App(tk.Tk):
 
         self.lower_frame.pack(padx=10, pady=10)
 
+        # 計算結果保持のための変数を用意
+        self.output_strings = ""
+        self.most_recent_strings = ""
+
     def clicked(self, event):
         text = event.widget["text"]
 
-        if text == "AC":
+        # 受け取ったボタンのtextに応じた処理を行う
+        if text == "AC":  # 全文字を消去する
             self.most_recent_strings = self.output_strings
             self.output_strings = ""
             self.text.set("")
 
-        elif text == "BS":
+        elif text == "BS":  # 右端の桁を削除する
             self.most_recent_strings = self.output_strings
             self.output_strings = self.output_strings[:-1]
             self.text.set(self.output_strings)
 
-        elif text == "=":
+        elif text == "=":  # 計算結果を表示する
             self.most_recent_strings = self.output_strings
             self.output_strings = str(eval(self.output_strings))
             self.text.set(self.output_strings)
 
-        elif text == "Undo":
+        elif text == "Undo":  # 一動作前の表示に戻す
             self.output_strings = self.most_recent_strings
             self.text.set(self.output_strings)
 
-        else:
+        else:  # 入力された数字、算術記号を表示させる
             self.most_recent_strings = self.output_strings
             self.output_strings += text
             self.text.set(self.output_strings)
